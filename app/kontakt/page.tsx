b63e0cc19e8ec37f46cd4b5cd9d01938982977a5
Marek Daněk <MarekDanek05@seznam.cz>
2026-02-26 14:17:08 +0100

    01-zakladni-stranky

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,8 @@
+export default function Kontakt() {
+  return (
+    <main>
+      <h1>Kontaktujte nás</h1>
+      <p>Napište nám na email: test@test.cz</p>
+    </main>
+  );
+}
---
+++ app/o-nas/page.tsx
@@ -0,0 +1,8 @@
+export default function ONas() {
+  return (
+    <main>
+      <h1>O nás</h1>
+      <p>Jsme studenti, co se učí tvořit weby v Next.js!</p>
+    </main>
+  );
+}
